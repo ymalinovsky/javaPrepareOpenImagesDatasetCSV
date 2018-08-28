ce0ad1053d5f05f1b179567fd584212b799c7913
artem <tyomasson@yandex.ru>
2018-08-28 19:07:57 +0300

    getTuricreateImagesData

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,17 @@ import org.apache.commons.io.*;
 public class Main {
 
     private final static String HUMAN_HAND_ID = "/m/0k65p";
-    private final static String PATH_TO_IMAGES_FOLDER = "/Users/artem/Downloads/Open_Images_Datase/train/";
+    private final static String PATH_TO_IMAGES_FOLDER = "/Users/artem/Downloads/Open_Images_Datase/train/data/";
 
     public static void main(String[] args) throws IOException {
-        ArrayList<String> imagesListWithHand = getImagesListWithHand();
+        ArrayList<String> imageListWithHand = getImageListWithHand();
+        Map<String, Map> turicreateImagesData = getTuricreateImagesData(imageListWithHand);
 
         System.out.println("ATATA!!!");
     }
 
-    private static ArrayList<String> getImagesListWithHand() throws IOException {
-        ArrayList<String> imagesListWithHand = new ArrayList<String>();
+    private static ArrayList<String> getImageListWithHand() throws IOException {
+        ArrayList<String> imageListWithHand = new ArrayList<String>();
 
         Reader in = new FileReader("/Users/artem/Downloads/Open_Images_Datase/train/imageLabels.csv");
         Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
@@ -28,23 +29,62 @@ public class Main {
             String confidence = record.get("Confidence");
 
             if (source.equals("verification") && confidence.equals("1") && labelName.equals(HUMAN_HAND_ID)) {
-                imagesListWithHand.add(imageID);
+                imageListWithHand.add(imageID);
             }
 
         }
 
-        return imagesListWithHand;
+        return imageListWithHand;
     }
 
-    private static void saveFileToFolderImages(String imageStringURL) throws IOException {
+    private static Map<String, Map> getTuricreateImagesData(ArrayList<String> imageListWithHand) throws IOException {
+        Map<String, Map> turicreateImagesData = new HashMap<String, Map>();
 
+
+        Reader in = new FileReader("/Users/artem/Downloads/Open_Images_Datase/train/imageIDs.csv");
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
+        for (CSVRecord record : records) {
+            String imageID = record.get("ImageID");
+            String originalURL = record.get("OriginalURL");
+
+            for (String openImageWithHandID : imageListWithHand) {
+                if (openImageWithHandID.equals(imageID)) {
+                    String filename = getFilePathAndSaveFileToFolderImages(originalURL);
+
+                    if (filename != null) {
+                        Map<String, String> turicreateImageData = new HashMap<String, String>();
+                        turicreateImageData.put("path", String.format("%s%s", "data/", filename));
+
+                        turicreateImagesData.put(openImageWithHandID, turicreateImageData);
+                    }
+                }
+            }
+        }
+
+
+        return turicreateImagesData;
+    }
+
+    private static String getFilePathAndSaveFileToFolderImages(String imageStringURL) throws IOException {
         InputStream in = new URL(imageStringURL).openStream();
         try {
             String filename = FilenameUtils.getName(imageStringURL);
+            String filePath = String.format("%s%s", PATH_TO_IMAGES_FOLDER, filename);
 
-            FileOutputStream out = new FileOutputStream(String.format("%s%s", PATH_TO_IMAGES_FOLDER, filename));
-            out.write(IOUtils.toByteArray(in));
-            out.close();
+            File file = new File(filePath);
+            if (!file.exists() && !file.isDirectory()) {
+                byte[] fileByte = IOUtils.toByteArray(in);
+
+                if (fileByte.length < 5000) {
+                    return null;
+                } else {
+                    FileOutputStream out = new FileOutputStream(filePath);
+                    out.write(fileByte);
+                    out.close();
+                }
+            }
+
+            return filename;
         } finally {
             in.close();
         }
